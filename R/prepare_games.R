cf391ee4dcd036a92584dcc20dbd9c5d0cfb667c
Fabio Carbone <fabio.carbone@unibas.ch>
2023-09-15 09:27:06 +0000

    saving

--- R/prepare_games.R
+++ R/prepare_games.R
@@ -17,3 +17,29 @@ games_raw <- readr::read_csv(
 # (i.e., variable names are documentation + comments)
 games <- mutate(games_raw, date = str_c(month, "_", year) %>% # concatenate month and year
                   my()) # my = month year -> convert to date
+
+
+# use a function without loading the package:
+# package::function
+usethis::use_git_config(
+  user.name = "Fabio Carbone", # <-- change to your name
+  user.email = "fabio.carbone@unibas.ch", # <-- and your email
+  init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
+
+usethis::use_git()
+2
+
+usethis::create_github_token(description = "Token for Repro Workshop 2023 Test")
+gitcreds::gitcreds_set() # <-- Token must *not* go into brackets, paste when asked
+usethis::use_github()
+
+
+library(repro)
+automate()
+use_
+use_gha_docker()
+library(usethis)
+browse
+browse_github_actions()
+
+use_make_publish()
